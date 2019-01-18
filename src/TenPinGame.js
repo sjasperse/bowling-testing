0e1ab53218416108420d59fc61b26aa7d14e9243
Scott Jasperse <scott.jasperse@spectrumhealth.org>
2019-01-18 15:58:43 -0500

    Expanding rules and test coverage

--- src/TenPinGame.js
+++ src/TenPinGame.js
@@ -1,5 +1,6 @@
 module.exports = function TenPinGame() {
   let pinsRemaining = 10;
+  let finished = false;
   const frames = [ new Frame(1) ];
 
   return {
@@ -12,10 +13,20 @@ module.exports = function TenPinGame() {
       return currentFrame();
     },
     get currentBall() {
+      if (finished) {
+        return currentFrame().balls.length;
+      }
+
       return currentFrame().balls.length + 1;
     },
     get score() {
       return getScore();
+    },
+    get finished() {
+      return finished;
+    },
+    get frameSummary() {
+      return getFrameSummary();
     }
   };
 
@@ -35,20 +46,42 @@ module.exports = function TenPinGame() {
       throw new Error('Liar. That\'s more pins than were up.');
     }
 
+    const _currentFrame = currentFrame();
+
     pinsRemaining -= pinsKnockedDown;
 
-    currentFrame().balls.push(pinsKnockedDown);
+    _currentFrame.balls.push(pinsKnockedDown);
+    const currentBall = _currentFrame.balls.length;
 
-    var resultMessage = (() => {
+    var resultMessage = (function () {
       switch (pinsKnockedDown) {
         case 0: return 'Gutterball!';
-        case 10: return 'Strike!'
+        case 10: return 'Strike!';
+        default:
+          if (pinsRemaining === 0) {
+            return 'Spare!';
+          }
+
+          return pinsKnockedDown;
       }
     })();
 
-    if (pinsRemaining === 0) {
-      frames.push(new Frame(currentFrame().number + 1));
-      pinsRemaining = 10;
+    if (_currentFrame.number === 10) {
+      if (currentBall === 1 || currentBall === 2 && pinsRemaining === 0) {
+        // keep going
+
+        if (pinsRemaining === 0) {
+          pinsRemaining = 10;
+        }
+      } else {
+        finished = true;
+      }
+    }
+    else {
+      if (pinsRemaining === 0 || _currentFrame.balls.length === 2) {
+        frames.push(new Frame(_currentFrame.number + 1));
+        pinsRemaining = 10;
+      }
     }
 
     return {
@@ -56,8 +89,63 @@ module.exports = function TenPinGame() {
     }
   }
 
-  function getScore() {
-    return frames.reduce((p,x) => p + x.balls.reduce((p2, x2) => p2 + x2, 0), 0);
+  function getScore(endingFrame = 10) {
+    let _endingFrame = Math.min(frames.length, endingFrame);
+    let scoreAccum = 0;
+    for (let frameIndex = 0; frameIndex < _endingFrame; frameIndex++) {
+      const frame = frames[frameIndex];
+      for(let ball = 0; ball < frame.balls.length; ball++) {
+        const pins = frame.balls[ball];
+
+        if (frame.number === 10) {
+          scoreAccum += pins;
+        }
+        else if (ball === 0 && pins === 10) {
+          const futureScore = getScoreFromNextXBalls(2);
+          scoreAccum += (pins + futureScore);
+        }
+        else if (ball === 1 && pins + frame.balls[0] === 10) {
+          const futureScore = getScoreFromNextXBalls(1);
+          scoreAccum += (pins + futureScore);
+        } else {
+          scoreAccum += pins;
+        }
+
+        function getNextXBalls(x) {
+          const r = [];
+          for (let frameOffset = 0; frameIndex + frameOffset < frames.length; frameOffset++) {
+            const futureFrame = frames[frameIndex + frameOffset];
+
+            for (let futureBall = 0; futureBall < futureFrame.balls.length; futureBall++) {
+              if (frameOffset === 0 && futureBall <= ball) { continue; }
+
+              const futurePins = futureFrame.balls[futureBall];
+
+              r.push(futurePins);
+
+              if (r.length === x) {
+                return r;
+              }
+            }
+          }
+
+          return r;
+        }
+        function getScoreFromNextXBalls(x) {
+          return getNextXBalls(x).reduce((p, x) => p + x, 0);
+        }
+      }
+    }
+
+    return scoreAccum;
+  }
+
+  function getFrameSummary() {
+    return frames
+      .map(f => ({
+        balls: f.balls,
+        score: getScore(f.number)
+      }));
   }
 
   function Frame(number) {
--- src/TenPinGame.tests.js
+++ src/TenPinGame.tests.js
@@ -44,9 +44,22 @@ describe('TenPinGame', () => {
       itShouldHaveACurrentFrameOf(1);
       itShouldHaveACurrentBallOf(2);
       itShouldHave(7).pinsRemaining();
+      itShouldGiveMeAMessageMatching(/3/i);
+
+      describe('and then I bowl a spare', () => {
+        beforeEach(() => {
+          ballResult = game.bowledAndKnockedDown(7);
+        });
+
+        itShouldHaveAScoreOf(10);
+        itShouldHaveACurrentFrameOf(2);
+        itShouldHaveACurrentBallOf(1);
+        itShouldHave(10).pinsRemaining();
+        itShouldGiveMeAMessageMatching(/spare/i);
+      });
     });
     
-    describe('when I bowl and knock down 10 pins', () => {
+    describe('when I bowl a strike', () => {
       beforeEach(() => {
         ballResult = game.bowledAndKnockedDown(10);
       });
@@ -56,6 +69,196 @@ describe('TenPinGame', () => {
       itShouldHaveACurrentBallOf(1);
       itShouldHave(10).pinsRemaining();
       itShouldGiveMeAMessageMatching(/strike/i);
+    });
+
+    describe('when I bowl three frames of gutterballs', () => {
+      beforeEach(() => {
+        // frame 1
+        game.bowledAndKnockedDown(0);
+        game.bowledAndKnockedDown(0);
+
+        // frame 2
+        game.bowledAndKnockedDown(0);
+        game.bowledAndKnockedDown(0);
+
+        // frame 3
+        game.bowledAndKnockedDown(0);
+        game.bowledAndKnockedDown(0);
+      });
+
+      itShouldHaveAScoreOf(0);
+      itShouldHaveACurrentFrameOf(4);
+      itShouldHaveACurrentBallOf(1);
+      itShouldHave(10).pinsRemaining();
+    });
+
+    describe('when I bowl three frames of 2s', () => {
+      beforeEach(() => {
+        // frame 1
+        game.bowledAndKnockedDown(2);
+        game.bowledAndKnockedDown(2);
+
+        // frame 2
+        game.bowledAndKnockedDown(2);
+        game.bowledAndKnockedDown(2);
+
+        // frame 3
+        game.bowledAndKnockedDown(2);
+        game.bowledAndKnockedDown(2);
+      });
+
+      itShouldHaveAScoreOf(12);
+      itShouldHaveACurrentFrameOf(4);
+      itShouldHaveACurrentBallOf(1);
+      itShouldHave(10).pinsRemaining();
+    });
+
+    describe('It should evaluate strike scores properly', () => {
+      describe('When I bowl three strikes', () => {
+        beforeEach(() => {
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+        });
+  
+        itShouldHaveAScoreOf(60);
+        itShouldHaveACurrentFrameOf(4);
+        itShouldHaveACurrentBallOf(1);
+        itShouldHave(10).pinsRemaining();
+      });
+  
+      describe('When I bowl three strikes, and then a 2', () => {
+        beforeEach(() => {
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(2);
+        });
+  
+        itShouldHaveAScoreOf(66);
+        itShouldHaveACurrentFrameOf(4);
+        itShouldHaveACurrentBallOf(2);
+        itShouldHave(8).pinsRemaining();
+      });
+  
+      describe('When I bowl three strikes, and then 2 2s', () => {
+        beforeEach(() => {
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(10);
+          game.bowledAndKnockedDown(2);
+          game.bowledAndKnockedDown(2);
+        });
+  
+        itShouldHaveAScoreOf(70);
+        itShouldHaveACurrentFrameOf(5);
+        itShouldHaveACurrentBallOf(1);
+        itShouldHave(10).pinsRemaining();
+      });
+    })
+
+    describe('It should evaluate spare scores properly', () => {
+      describe('When I bowl two spares in a row', () => {
+        beforeEach(() => {
+          game.bowledAndKnockedDown(2);
+          game.bowledAndKnockedDown(8);
+          game.bowledAndKnockedDown(2);
+          game.bowledAndKnockedDown(8);
+        });
+  
+        itShouldHaveAScoreOf(22);
+        itShouldHaveACurrentFrameOf(3);
+        itShouldHaveACurrentBallOf(1);
+        itShouldHave(10).pinsRemaining();
+      });
+    })
+
+    describe('it should evaluate the 10th frame correctly', () => {
+      describe('in the 10th frame', () => {
+        beforeEach(() => {
+          // 9 empty frames
+          for(let i =0; i < 9; i++) {
+            game.bowledAndKnockedDown(0);
+            game.bowledAndKnockedDown(0);
+          }
+        });
+
+        describe('when I bowl 2 2s', () => {
+          beforeEach(() => {
+            game.bowledAndKnockedDown(2);
+            game.bowledAndKnockedDown(2);
+          });
+  
+          itShouldHaveAScoreOf(4);
+          itShouldHaveACurrentFrameOf(10);
+          itShouldHaveACurrentBallOf(2);
+          itShouldHave(6).pinsRemaining();
+          itShouldShowTheGameIsFinished();
+        });
+
+        describe('when I bowl a 2 and an 8 (for a spare)', () => {
+          beforeEach(() => {
+            game.bowledAndKnockedDown(2);
+            game.bowledAndKnockedDown(8);
+          });
+  
+          itShouldHaveAScoreOf(10);
+          itShouldHaveACurrentFrameOf(10);
+          itShouldHaveACurrentBallOf(3);
+          itShouldHave(10).pinsRemaining();
+          itShouldShowTheGameIsNotFinished();
+
+          describe('and then I bowl a strike', () => {
+            beforeEach(() => {
+              game.bowledAndKnockedDown(10);
+            });
+  
+            itShouldHaveAScoreOf(20);
+            itShouldHaveACurrentFrameOf(10);
+            itShouldHaveACurrentBallOf(3);
+            itShouldHave(0).pinsRemaining();
+            itShouldShowTheGameIsFinished();
+          });  
+        });
+
+        describe('when I bowl a strike', () => {
+          beforeEach(() => {
+            game.bowledAndKnockedDown(10);
+          });
+  
+          itShouldHaveAScoreOf(10);
+          itShouldHaveACurrentFrameOf(10);
+          itShouldHaveACurrentBallOf(2);
+          itShouldHave(10).pinsRemaining();
+          itShouldShowTheGameIsNotFinished();
+
+          describe('and then I bowl a 2', () => {
+            beforeEach(() => {
+              game.bowledAndKnockedDown(2);
+            });
+  
+            itShouldHaveAScoreOf(12);
+            itShouldHaveACurrentFrameOf(10);
+            itShouldHaveACurrentBallOf(2);
+            itShouldHave(8).pinsRemaining();
+            itShouldShowTheGameIsFinished();
+          });  
+        });
+      });
+    });
+
+    describe('when I bowl 12 strikes', () => {
+      beforeEach(() => {
+        for(let i =0; i < 12; i++) {
+          game.bowledAndKnockedDown(10);
+        }
+      });
+
+      itShouldHaveAScoreOf(300);
+      itShouldHaveACurrentFrameOf(10);
+      itShouldHaveACurrentBallOf(3);
+      itShouldHave(0).pinsRemaining();
+      itShouldShowTheGameIsFinished();
     });
   });
 });
@@ -92,5 +295,17 @@ function itShouldHave(number) {
 function itShouldGiveMeAMessageMatching(regex) {
   it('should give me a message matching ${regex}', () => {
     expect(ballResult.message).to.match(regex);
+  });
+}
+
+function itShouldShowTheGameIsFinished() {
+  it('should show that the game is finished', () => {
+    expect(game.finished).to.be.true;
+  });
+}
+
+function itShouldShowTheGameIsNotFinished() {
+  it('should show that the game is not finished', () => {
+    expect(game.finished).to.be.false;
   });
 }
